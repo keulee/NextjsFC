f2346ef21d6b7bf5b3ce290dfbff7cfad5dd2030
keulee <klee8903@gmail.com>
2023-08-09 02:29:30 +0200

    index page updated

--- components/tweet.tsx
+++ components/tweet.tsx
@@ -1,1 +1,49 @@
-export default function Tweet() {}
+import Link from "next/link";
+
+interface argType {
+  title: string;
+  id: number;
+  hearts: number;
+}
+
+export default function Tweet({ title, id, hearts }: argType) {
+  return (
+    <Link
+      legacyBehavior
+      className="flex px-4 pt-5 cursor-pointer justify-between"
+      href={`/tweets/${id}`}
+    >
+      <a className="border-4 border-sky-500 rounded-lg">
+        {/* <div className="border-4 rounded-lg w-96 h-32 pt-1 px-1 border-sky-500 cursor-pointer">
+          <div>title: {title}</div>
+        </div> */}
+        <div className="flex space-x-4">
+          {/* <div className="w-20 h-20 bg-sky-500 rounded-md" /> */}
+          <div className="pt-2 flex flex-col">
+            <h3 className="text-2xl font-medium text-gray-900 pl-2">"hello"</h3>
+            <span className="font-base mt-1 text-gray-900 pl-2">"Tag"</span>
+          </div>
+        </div>
+        <div className="flex space-x-2 items-end justify-end">
+          <div className="flex space-x-0.5 items-center text-sm  text-gray-600">
+            <svg
+              className="w-4 h-4"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
+              ></path>
+            </svg>
+            <span className="pr-2 pb-2">"hearts"</span>
+          </div>
+        </div>
+      </a>
+    </Link>
+  );
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import React from "react";
+import Tweet from "../components/tweet";
 
 //로그인 여부를 확인하여 로그인이 되어있다면 홈페이지를 그렇지 않다면 계정 생성 / 로그인 페이지로 이동하세요.
 //After logging in, in the Home Page, the user should see all the Tweets on the database, the user should also be able to POST a Tweet.
@@ -11,5 +12,36 @@ export default () => (
     <Head>
       <title>My Mini Tweet | MiNi X Tweet</title>
     </Head>
+    <div className="flex flex-col items-center mt-10 font-semibold">
+      <div className="text-8xl">Hi there !</div>
+      <div>Let theme hear your stories and connect with others :D</div>
+    </div>
+    <div className="flex flex-col space-y-5 py-10 px-96">
+      {[1, 1, 1, 1, 1, 1].map((product) => (
+        <Tweet
+          id={product.id}
+          key={product.id}
+          title="hello world"
+          hearts={1}
+        />
+      ))}
+      {/* <FloatingButton href="/products/upload">
+        <svg
+          className="h-6 w-6"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+          aria-hidden="true"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+            d="M12 6v6m0 0v6m0-6h6m-6 0H6"
+          />
+        </svg>
+      </FloatingButton> */}
+    </div>
   </div>
 );
