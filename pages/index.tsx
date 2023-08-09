22c09e12890a58d9856d8e726d3ba46fe522038a
keulee <klee8903@gmail.com>
2023-08-09 18:52:09 +0200

    user button svg added

--- pages/index.tsx
+++ pages/index.tsx
@@ -71,14 +71,19 @@ export default () => {
         <ButtonProfile href={`/users/profiles/${user?.id}`}>
           <svg
             className="h-10 w-10"
-            viewBox="0 0 24 24"
-            fill="white"
+            data-name="Layer 1"
+            id="Layer_1"
+            viewBox="0 0 32 32"
             xmlns="http://www.w3.org/2000/svg"
+            fill="white"
           >
             <path
-              d="M 12 2.0996094 L 1 12 L 4 12 L 4 21 L 11 21 L 11 15 L 13 15 L 13 21 L 20 21 L 20 12 L 23 12 L 12 2.0996094 z M 12 4.7910156 L 18 10.191406 L 18 11 L 18 19 L 15 19 L 15 13 L 9 13 L 9 19 L 6 19 L 6 10.191406 L 12 4.7910156 z"
+              d="M16,20a8,8,0,1,1,8-8A8,8,0,0,1,16,20ZM16,6a6,6,0,1,0,6,6A6,6,0,0,0,16,6Z"
               stroke="white"
-              strokeWidth="1"
+            />
+            <path
+              d="M30,32H28A12,12,0,0,0,4,32H2a14,14,0,0,1,28,0Z"
+              stroke="white"
             />
           </svg>
         </ButtonProfile>
