76c52e05a0bfc819b8df4800d22b5e3f3763178e
keulee <klee8903@gmail.com>
2023-08-12 23:27:43 +0200

    profile ing

--- pages/users/profiles/[id].tsx
+++ pages/users/profiles/[id].tsx
@@ -1,24 +1,35 @@
 import useSWR from "swr";
+import useUser from "../../../lib/useUser";
 
-// interface userType {
-//   data: any;
-//   isLoading: boolean;
-// }
 export default function Profile() {
-  const { data, isLoading } = useSWR("/api/users/user");
-  console.log("here->", data);
+  const { user, isLoading } = useUser();
+  console.log("here->", user?.name);
   // const {detail } =useSWr("")
 
+  const handleLogout = (e) => {
+    // console.log(e.tar);
+  };
   return (
     <div>
-      <div>{data?.profile?.name}</div>
-      <div>{data?.profile?.email}</div>
-      {/* <div>
-        {data?.profile?.Fav?.map((post) => {
-          <p>{post}</p>;
-        })}
-      </div> */}
-      <button>Log out</button>
+      <div className="flex flex-col gap-y-10 h-full w-full">
+        <div className="flex justify-center gap-x-10 mt-10">
+          <div className="w-48 h-48 rounded-full bg-slate-500" />
+          <div className="flex flex-col justify-end font-semibold">
+            <div className="text-2xl">{user?.name}</div>
+            <div>{user?.email}</div>
+          </div>
+        </div>
+        <div>My favorite tweets</div>
+        <div className="grid">
+          {user?.Fav?.map((tweet) => (
+            <div key={tweet.id}>
+              <p>{tweet.post.title}</p>
+              <p>{tweet.post.text}</p>
+            </div>
+          ))}
+        </div>
+        <button onClick={handleLogout}>Log out</button>
+      </div>
     </div>
   );
 }
